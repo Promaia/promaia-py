33cc1cb597e2612bd01bf5ded9a3846b59dd71b2
Rose <rose@promaia.com>
2026-03-09 23:15:13 -0400

    fix chroma db path

--- promaia/storage/hybrid_storage.py
+++ promaia/storage/hybrid_storage.py
@@ -1225,7 +1225,7 @@ class HybridContentRegistry:
 
             # Initialize vector DB
             from promaia.storage.vector_db import VectorDBManager
-            vector_db = VectorDBManager(chroma_path=vector_config.get('chroma_path', 'chroma_db'))
+            vector_db = VectorDBManager(chroma_path=vector_config.get('chroma_path'))
 
             # Get property schema
             property_schema = self.get_property_schema(database_id)
@@ -1390,7 +1390,7 @@ class HybridContentRegistry:
             
             # Initialize vector DB
             from promaia.storage.vector_db import VectorDBManager
-            vector_db = VectorDBManager(chroma_path=vector_config.get('chroma_path', 'chroma_db'))
+            vector_db = VectorDBManager(chroma_path=vector_config.get('chroma_path'))
             
             # Prepare metadata
             metadata = {
@@ -2473,7 +2473,7 @@ class HybridContentRegistry:
 
                         if vector_config.get('enabled') and vector_config.get('property_embeddings', {}).get('enabled'):
                             from promaia.storage.vector_db import VectorDBManager
-                            vector_db = VectorDBManager(chroma_path=vector_config.get('chroma_path', 'chroma_db'))
+                            vector_db = VectorDBManager(chroma_path=vector_config.get('chroma_path'))
 
                             EMBEDDABLE_TYPES = {'title', 'text', 'rich_text', 'relation'}
 
@@ -2500,7 +2500,7 @@ class HybridContentRegistry:
 
                         if vector_config.get('enabled') and vector_config.get('property_embeddings', {}).get('enabled'):
                             from promaia.storage.vector_db import VectorDBManager
-                            vector_db = VectorDBManager(chroma_path=vector_config.get('chroma_path', 'chroma_db'))
+                            vector_db = VectorDBManager(chroma_path=vector_config.get('chroma_path'))
 
                             EMBEDDABLE_TYPES = {'title', 'text', 'rich_text', 'relation'}
 
